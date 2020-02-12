d42571a38e80e8c7800d9299c5c51b7b9f222fc0
RarCeth <rarceth@gmail.com>
2020-02-13 07:11:41 +0800

    init

---
+++ Assets/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Start_GenerateLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Start_GenerateLevel ()
+    {
+        StartCoroutine(GetComponent<LevelGenerator>().GenerateLevel());
+    }
+
+
+}
---
+++ Assets/NeighbourHolder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighbourHolder : MonoBehaviour
+{
+    public List<GameObject> neighbours = new List<GameObject>();
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        if (neighbours != null && neighbours.Count > 0)
+        {
+            foreach (GameObject t in neighbours)
+            {
+                Gizmos.DrawLine(transform.position, t.transform.position);
+            }
+        }
+    }
+}
